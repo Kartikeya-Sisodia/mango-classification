6e27b88c3c7809d2cd8cfcddcdee7c2dbac3b533
Kartikeya-Sisodia <109451766+Kartikeya-Sisodia@users.noreply.github.com>
2022-07-19 07:17:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -168,7 +168,7 @@ function checkImageWithNyckel(image)
     formdata.append('file', image);
 
     $.ajax({
-        url:'https://www.nyckel.com/v1/functions/pxvrvn451z7vtd44/invoke',
+        url:'https://www.nyckel.com/v1/functions/upfolx6337o94azh/invoke',
         type: 'post',
         data: formdata,
         contentType: false,
